c78b55b756e485c37fc80d8bf062fcb179abae8e
Sanket Bande <sanketbande108@gmail.com>
2026-09-04 09:50:45 +0530

    Add system health assessment

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import psutil
 
+def check_health(value):
+    if value < 70:
+        return "HEALTHY"
+    elif value <= 90:
+        return "WARNING"
+    else:
+        return "CRITICAL"
+
 cpu_usage = psutil.cpu_percent(interval=1)
 memory_usage = psutil.virtual_memory().percent
 disk_usage = psutil.disk_usage("/").percent
 
-print("========= NetGuardian ================")
-print("CPU Usage:",cpu_usage, "%")
-print("Memory Usage:", memory_usage, "%")
-print("Disk Usage:", disk_usage, "%")
-print(" =====================================")
+cpu_status = check_health(cpu_usage)
+memory_status = check_health(memory_usage)
+disk_status = check_health(disk_usage)
+
+
+print("========== NetGuardian ==========")
+print("CPU Usage:", cpu_usage, "%", "-", cpu_status)
+print("Memory Usage:", memory_usage, "%", "-", memory_status)
+print("Disk Usage:", disk_usage, "%", "-", disk_status)
+print("=================================")
+
+
