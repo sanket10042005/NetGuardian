2b4a8f4814ebb70893e610d44a05cb4372897258
Sanket Bande <sanketbande108@gmail.com>
2026-09-05 21:15:36 +0530

    Add overall system health

--- health.py
+++ health.py
@@ -5,3 +5,12 @@ def check_health(value):
         return "WARNING"
     else:
         return "CRITICAL"
+
+
+def get_overall_health(statuses):
+    if "CRITICAL" in statuses:
+        return "CRITICAL"
+    elif "WARNING" in statuses:
+        return "WARNING"
+    else:
+        return "HEALTHY"
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from system_monitor import get_system_metrics, get_system_info
-from health import check_health
+from health import check_health, get_overall_health
 
 
 def display_report(
@@ -11,9 +11,11 @@ def display_report(
     disk_usage,
     cpu_status,
     memory_status,
-    disk_status
+    disk_status,
+    overall_status
 ):
     print("========== NetGuardian ==========")
+
     print("System Information")
     print("OS:", operating_system)
     print("Kernel:", os_version)
@@ -24,15 +26,29 @@ def display_report(
     print("CPU Usage:", cpu_usage, "%", "-", cpu_status)
     print("Memory Usage:", memory_usage, "%", "-", memory_status)
     print("Disk Usage:", disk_usage, "%", "-", disk_status)
+
+    print()
+    print("Overall System Health:", overall_status)
+
     print("=================================")
 
 
+
 cpu_usage, memory_usage, disk_usage = get_system_metrics()
+
+
 operating_system, os_version, hostname = get_system_info()
+
 
 cpu_status = check_health(cpu_usage)
 memory_status = check_health(memory_usage)
 disk_status = check_health(disk_usage)
+
+
+overall_status = get_overall_health(
+    [cpu_status, memory_status, disk_status]
+)
+
 
 display_report(
     operating_system,
@@ -43,5 +59,6 @@ display_report(
     disk_usage,
     cpu_status,
     memory_status,
-    disk_status
+    disk_status,
+    overall_status
 )
